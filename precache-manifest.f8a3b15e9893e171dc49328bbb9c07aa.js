676b77ff80ba73956e09b61457b7e014c9c7c602
tommybernaciak <tommy@tommybernaciak.com>
2019-07-28 11:50:10 +0200

    Updates

---
+++ precache-manifest.f8a3b15e9893e171dc49328bbb9c07aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c011a004e49097616b08659937df39a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f5cc06d5887f1c6b62",
+    "url": "/static/css/2.388ae2a1.chunk.css"
+  },
+  {
+    "revision": "e10bfc93072ea1ce33f7",
+    "url": "/static/css/main.2b28fbda.chunk.css"
+  },
+  {
+    "revision": "b5f5cc06d5887f1c6b62",
+    "url": "/static/js/2.79254e39.chunk.js"
+  },
+  {
+    "revision": "e10bfc93072ea1ce33f7",
+    "url": "/static/js/main.6f2e986f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/static/media/icons-20.90247cfd.ttf"
+  }
+]);
