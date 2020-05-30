d4000cfa1157e24ab4dd336aa2401c089bcddf61
tommybernaciak <tommy@tommybernaciak.com>
2020-05-30 14:26:42 +0200

    Updates

---
+++ precache-manifest.608b3a29b9dff79f58901ae38effea2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73c97421b96e80db0b8f0363061ec88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "873baa7ed859f37ba8e7",
+    "url": "/static/css/main.2d94de0e.chunk.css"
+  },
+  {
+    "revision": "586209a3f722511ed32d",
+    "url": "/static/js/2.2cee4302.chunk.js"
+  },
+  {
+    "revision": "873baa7ed859f37ba8e7",
+    "url": "/static/js/main.56e214f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11a05806fdf2aa7121614f227fdc03f7.js"
+  "/precache-manifest.608b3a29b9dff79f58901ae38effea2c.js"
 );
 
 self.addEventListener('message', (event) => {
